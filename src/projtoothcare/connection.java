b804a7e9309ae3e9c6815e793ef7cc8416d79e0e
Mufli-Mohideen <mufli2mail@gmail.com>
2024-08-29 15:27:59 +0530

    first commit

---
+++ src/projtoothcare/connection.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+package projtoothcare;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+/**
+ *
+ * @author MUFLY
+ */
+public class connection {
+    private static final String URL = "jdbc:mysql://localhost:3306/toothcaredb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static PreparedStatement prepareStatement(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
